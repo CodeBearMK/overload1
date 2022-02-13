2f82d2c9a80134263364054ca488cc0e7b6b5ed6
cat32tw <mike1993xd@gmail.com>
2022-02-13 16:29:25 +0800

    方法多載練習

---
+++ CMath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overload1
+{
+    internal class CMath
+    {
+        public void GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                Console.WriteLine("{0},{1} 較大值為{1}", num2, num1);
+            }
+            else
+            {
+                Console.WriteLine("{0},{1} 較大值為{1}", num1, num2);
+            }
+        }
+
+        public void GetMax(int[] numAry)
+        {
+            string outputstr = "";
+            Array.Sort(numAry);
+            for (int i = 0; i < numAry.Length; i++)
+            {
+                if (i != numAry.Length - 1)
+                {
+                    outputstr += numAry[i].ToString() + ",";
+                }
+                else
+                {
+                    outputstr += numAry[i].ToString();
+                }
+                
+            }
+            Console.WriteLine("{0} 中最大值為 {1}", outputstr, numAry[numAry.Length-1]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace overload1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 50, num2 = 30;
+            int[] numAry = { 10, 20, 30, 70, 50 };
+
+            CMath numberCal = new CMath();
+            numberCal.GetMax(num1, num2);
+            numberCal.GetMax(numAry);
+            Console.Read();
+        }
+    }
+}
